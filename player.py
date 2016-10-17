95478d6ea2363b853b3763d97f3ed5e10eb3ff57
Clement <clement.julia13@gmail.com>
2016-10-17 15:11:26 +0100

    removed import

--- player.py
+++ player.py
@@ -2,4 +2,3 @@ player_inventory=[]
 player_stats={"name":"Zorg", "class":"Dark Lord of Dorath", "health":500,"max_health":500, "mana":200,"max_mana":200, "experience":0, "level":20, "money":2, "evasion":70}
 questCounter = 0
 karmaCounter = 0
-current_room = rooms["town"]
