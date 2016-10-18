80662166b3c54f7b2785be7513e506279d8f6e8c
Clement <clement.julia13@gmail.com>
2016-10-18 10:31:27 +0100

    changed error message and fixed spelling mistake

--- adventure_map.py
+++ adventure_map.py
@@ -129,7 +129,7 @@ def event(loc):
         world['22']['event'] = True
    
     elif loc == '24':
-        print('As you approach the farm you see it was destoryed and burnt by a golbin raid, anger takes control of your mind and you swear upon the old and the new gods to destroy the golbin lair situated north-west of the farm.')
+        print('As you approach the farm you see it was destroyed and burnt by a golbin raid, anger takes control of your mind and you swear upon the old and the new gods to destroy the golbin lair situated north-west of the farm.')
     world[loc]['event'] = False #This kills the event trigger so that an event may only happen once unless made happen again.
             
 while True:
@@ -142,7 +142,7 @@ while True:
         is_valid_direction = True
         if command in world[oldloc]['FD']:
             is_valid_direction = False
-            print('wrong direction motherfucker')         
+            print('Sorry, what?')         
         else:
             if command == 'north':
                 y=int(y)+1
