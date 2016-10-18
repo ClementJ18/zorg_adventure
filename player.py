e93502983642025802fdc6cf9a1ef12c5105e8ad
Clement Julia <Clement Julia>
2016-10-18 12:22:59 +0100

    counters

--- player.py
+++ player.py
@@ -2,11 +2,7 @@ player_inventory=[]
 player_stats={"name":"Zorg", "class":"Dark Lord of Dorath", "health":500,"max_health":500, "mana":200,"max_mana":200, "experience":0, "level":1, "money":2, "evasion":70}
 questCounter = 0
 karmaCounter = 0
-<<<<<<< HEAD
 plainCounter = 0
 lakeCounter = 0
 forestCounter = 0 
-=======
-sidequestCounter = 0
 exp_before_next_level = 50
->>>>>>> f58b1eff3bd6aa23a21f8a79d1b9372b267bf06f
