a578f7a19a580354475a4941224f83b4699987c7
Clement <clement.julia13@gmail.com>
2016-10-18 11:16:13 +0100

    small changes

--- player.py
+++ player.py
@@ -2,6 +2,4 @@ player_inventory=[]
 player_stats={"name":"Zorg", "class":"Dark Lord of Dorath", "health":500,"max_health":500, "mana":200,"max_mana":200, "experience":0, "level":20, "money":2, "evasion":70}
 questCounter = 0
 karmaCounter = 0
-sidequestCounter = 0    
-
-
+sidequestCounter = 0
