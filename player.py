cbe440fff867194cdcc8ce4e58149040eeea45b3
Parin Ratboriharn <Parin Ratboriharn>
2016-10-18 15:26:51 +0100

    Changed player

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-player_stats={"name":"Zorg", "class":"Dark Lord of Dorath", "health":500,"max_health":500, "mana":200,"max_mana":200, "experience":0, "level":1, "money":0, "evasion":70,"forestCou
+player_stats={"name":"Zorg", "class":"Dark Lord of Dorath", "health":500,"max_health":500, "mana":200,"max_mana":200, "experience":0, "level":1, "money":0, "evasion":70,"forestCounter":0}
 player_inventory=["gold_coin"]
 questCounter = 0
 karmaCounter = 0
