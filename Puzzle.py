c5299df7c71ed423af5db7d88edff29331c19cc1
Clement <clement.julia13@gmail.com>
2016-10-19 23:31:35 +0100

    M-29

--- Puzzle.py
+++ Puzzle.py
@@ -1,6 +1,7 @@
 from player import *
 from test_combat import *
 from Player_Win import *
+import system
 my_Puzzle = " drop golden coin"
 
 def negative_ending():
@@ -9,9 +10,11 @@ def negative_ending():
     player_input = input(">").strip().lower()
     if player_input == "take":
         print("You take the gem and it's power surges through you. You see the entire world and learn of all it's secrets in an instant. Nothing can stop you know, the world is at your mercy. Or is it...")
+        player_win()
         #start adventure again with intro2
     elif player_input == "destroy":
         print("You brandish your sword and strike at the gem. Your sword shatters upon it and one of the projectiles wounds you. You decide to smash the gem with your bear hands but the moment you cease the gem, it tries to take control of you. For what seems an eternity you fight for your survival but in the end the weigh of all the evil you have caused crushes you and you become a mindless puppet of this evil force. Hopefully one will arise to stop you...")
+        player_win()
     else:
         print("Sorry, what?")
         negative_ending()
@@ -44,6 +47,9 @@ def boss_fight():
     fight()
     if (karmaCounter >= -1) and (karmaCounter <= 1):
       print("You have finally slained the evil Dark Lord, well played. His body lies in front of you, lifeless and yet still menacing. You exit the castle but do not have the courage to return to the town, the things you have done to achieve this victory leave a bitter taste in your mouth. You prefer to wrap your cape around you and leave into the sunset.")
+      player_win()
+
+
     elif karmaCounter > 1:
       print("You have finally slained the evil Dark Lord, well played. His body lies in front of you, lifeless and yet still menacing. You turn around, ready to leave and enjoy the peace. As you begin to walk away so suddenly hear a sound, like a rasping voice, whispering to you.\"Shre nazg golugranu kilmi nudur, Ombi Kuzddurbagu gundum-ishi bagu. (Where are you going warrior? Your reward already lays here.\" As you turn around you see that a gem has risen out of the lifeless body of the Dark Lord. What would you like to do?")
       postive_ending()
