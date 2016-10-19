31d21efe250354a18502521fbd154bca89ea4057
Luke Webb <Luke Webb>
2016-10-19 18:11:20 +0100

    Tidied up text

--- introduction.py
+++ introduction.py
@@ -3,6 +3,7 @@ from name import *
 
 def pick_player_class():
     player_class = input("I want to be a: ").strip().lower()
+    print()
     if player_class in ["warrior","a warrior"]:
         player_inventory.append("Sword of Azeroth")
         player_inventory.append("Light Armor")
@@ -51,9 +52,9 @@ def pick_player_class():
         print("Sorry, what?")
         pick_player_class()
 def player_stats_show():
-    print("You are now a", player_stats["class"])
-    print("Your name is",player_stats["name"])
-    print("You are level", player_stats["level"])
+    print("You are now a", str(player_stats["class"]) + ".")
+    print("Your name is", str(player_stats["name"]) + ".")
+    print("You are level", str(player_stats["level"]) + ".")
     print("You have",player_stats["health"],"/",player_stats["max_health"],"health.")
     print("You have",player_stats["experience"],"experience points.")
     if player_stats["class"] == "rogue":
@@ -64,11 +65,13 @@ def player_stats_show():
         print("You have",player_stats["rage"],"/",player_stats["max_rage"],"rage.")
     else:
         pass
-    print("You have", ', '.join(player_inventory))
+    print("You have", ', '.join(player_inventory) + ".")
+    print()
 
 def player_ready():
 
-    player_input = input("You are about to enter a world of adventure. Are you ready, brave warrior? ").strip().lower()
+    player_input = input("You are about to enter a world of adventure. Are you ready, brave adventurer? ").strip().lower()
+    print()
     if player_input == "yes":
         print("Well then, onwards. --- INITIATING ADVENTURE ---")
     elif player_input == "no":
@@ -79,16 +82,20 @@ def player_ready():
 
 intro_screen()
 print()
-input("--- PRESS ANY KEY TO START ---")
+input("                                  --- PRESS ANY KEY TO START ---")
+print()
 
-print("Welcome to Group 20's text adventure. This is a test of skill, wit and strenght through the land of Zorg. You are young adventurer and have just arrived in this country, you have nothing but your courage and brains and will, to battle monsters, rescue citizens in distress and oppose all evil. But be weary, for evil breeds evil and deeds unworthy of heroes will be punished.")
-print("Before we start, what's your name mighty hero?")
+print("Welcome to Group 20's text adventure. This is a test of skill, wit and strength through the land of Zorg. You are a young adventurer and have just \narrived in this country; you have nothing but your courage and brains and will to battle monsters, rescue citizens in distress and oppose all evil. \nBut be weary, for evil breeds evil and deeds unworthy of heroes will be punished.")
+print()
+print("Before we start, what's your name, mighty hero?")
 player_stats["name"] = input("My name is: ").strip()
+print()
 
 print(player_stats["name"],"? That is a name filled with promise of heroic deeds.")
-print("You can pick one of the three classes available. There is:")
+print()
+print("You can pick one of the three classes available. There are:")
 print("NAME                 |    WARRIOR              |    MAGE                |   ROGUE")
-print("Specificity          |    Increased Health     |    Increased Mana      |   Evasion")
+print("Speciality           |    Increased Health     |    Increased Mana      |   Evasion")
 print("Weapon               |    Sword                |    Staff               |   Dagger")
 print("Secondary Weapon     |    Shield               |    Magic               |   Bow")
 print("Ability              |    Damage Boost         |    Self-Heal, Fireball,|   Ranged Attack")
@@ -101,11 +108,11 @@ print()
 pick_player_class()
 player_ready()
 print()
-print("===TOWN===")
-print("The Town is embellished with cobbled and paved streets, there are also a number of natural baths. There are a mixture of victorian townhouses and there are a mix of semi-detached and detached houses. The shop is located in the North, the Plains of Lithlad in the East and the Tavern in the South. What would you like to do?")
+print("=== TOWN ===")
+print("The Town is embellished with cobbled and paved streets, there are also a number of natural baths. There are Victorian townhouses and a mixture of \nsemi-detached and detached houses. A shop is located to the NORTH, the Plains of Lithlad to the EAST and a tavern to the SOUTH. \nWhat would you like to do?")
 print("You can:")
 print("SHOW INVENTORY")
 print("SHOW STATS")
-print("GO SOUTH to the tavern.")
-print("GO NORTH to the shop.")
-print("GO EAST to the Plains of Lithlad.")
+print("GO SOUTH to THE PRANCING PONY TAVERN.")
+print("GO NORTH to STELLA'S SHOP.")
+print("GO EAST to the PLAINS OF LITHLAD.")
