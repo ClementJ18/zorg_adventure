98be76c874520405fc9550d2a07f81494cbb6735
Clement Julia <Clement Julia>
2016-10-19 12:21:29 +0100

    minor bug fixing

--- test_combat.py
+++ test_combat.py
@@ -180,7 +180,22 @@ def fight():#Fight module
     else:
         print('u win')
         clear_dict()
-        #givexp
+        if player_stats["experience"] >= exp_before_next_level:
+            
+            player_stats["experience"] = player_stats["experience"] - exp_before_next_level
+            
+            player_stats["level"] = player_stats["level"] + 1
+            
+            player_stats["max_health"] = player_stats["max_health"] + 25
+            player_stats["health"] = player_stats["health"] + 25
+            player_stats["max_mana"] = player_stats["max_mana"] + 10
+            player_stats["mana"] = player_stats["mana"] + 10
+            
+            exp_before_next_level = int(1.2 * exp_before_next_level)
+
+            print("You have leveled up! You are now level " + str(player_stats["level"]) + ".")
+        else:
+            pass
         #give stuff
         
             
